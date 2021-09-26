699d49417075ce5ec764370ad2a1a4956585d717
NAT2K15 <72643615+NAT2K15@users.noreply.github.com>
2021-09-26 16:24:19 -0400

    Add files via upload

--- config.lua
+++ config.lua
@@ -7,3 +7,8 @@ config.words = {
 }
 
 config.retrun_message = "[^3SYSTEM^0] You cannot say ^1%s^0 in this server!"
+
+
+config.webhook = "https://discord.com/api/webhooks/871959364334919700/RGBrR4FOSFhcDCMb_IfLkJvRH-sqEdL1Z30jAjtwez2Z1adIiUpvehZ8yHLY98FKvMxt"
+config.color = 000000
+config.avatar_url = "https://cdn.discordapp.com/attachments/863961046799876116/891781655080095774/logo.png"
--- server.lua
+++ server.lua
@@ -1,10 +1,12 @@
 AddEventHandler('chatMessage', function(source, name, message)
     CancelEvent()
+    local src = source
     local function_check, msg = chatblacklist(message)
     if not function_check then
       TriggerClientEvent('chatMessage', -1, name, { 255, 255, 255 }, message)
 	  else 
       TriggerClientEvent('chatMessage', -1, string.format(config.retrun_message, msg))
+      sendtodiscord(src, msg)
     end
 end)
 
@@ -25,4 +27,19 @@ function chatblacklist(str)
         end
     end
     return blacklist, word
+end
+
+function sendtodiscord(user, message)
+  local embed = {
+    {
+      ["color"] = config.color,
+      ["title"] = "**[#".. user .. "] " .. GetPlayerName(user) .."**",
+      ["description"] = message,
+      ["footer"] = {
+        ["text"] = "MADE BY NAT2K15#2951",
+      },
+    }
+  }
+
+  PerformHttpRequest(config.webhook, function(err, text, headers) end, 'POST', json.encode({username = GetPlayerName(user), embeds = embed, avatar_url = config.avatar_url}), { ['Content-Type'] = 'application/json' })
 end
